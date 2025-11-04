c3b0ac29a41f46df6868affc89ed61fe40d466c1
상만 <a01071397193@gmail.com>
2025-11-04 15:25:24 +0900

    feat: frotend template

---
+++ src/widgets/layouts/provider/index.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import {ReactNode, useState} from "react";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ToastContainer} from "react-toastify"
+
+type Props = { children: ReactNode };
+
+const Providers = ({children}: Props) => {
+    const [client] = useState(
+        () =>
+            new QueryClient({
+                defaultOptions: {
+                    queries: {retry: 1},
+                },
+            })
+    );
+
+    return (
+        <QueryClientProvider client={client}>
+            <ToastContainer/>
+            {children}
+        </QueryClientProvider>
+    );
+}
+
+export default Providers;
